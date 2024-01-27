4e1bc54efcae7021bcffe8c292524e7107906b13
Rajan Ravisaheb <rajanroy220@gmail.com>
2024-01-27 12:13:27 +0530

    context api added

--- src/contexts/ClientContext.jsx
+++ src/contexts/ClientContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const CitiesContext = createContext();
 const BASE_URL = "http://localhost:8000";
@@ -6,6 +7,7 @@ const BASE_URL = "http://localhost:8000";
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentcity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchData() {
@@ -25,8 +27,23 @@ function CitiesProvider({ children }) {
     fetchData();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+
+      const data = await res.json();
+      //console.log(data);
+      setCurrentCity(data);
+    } catch (err) {
+      alert("error while fetching data");
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, loading }}>
+    <CitiesContext.Provider value={{ cities, loading, currentcity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );
